f09096d25a75fba835de9f38c7f504f01bb56d1d
Rawkun <badexploits@gmail.com>
2023-12-16 20:08:54 +0100

    Iconname

--- cogs/listener.py
+++ cogs/listener.py
@@ -196,7 +196,7 @@ class Listener(commands.Cog):
                     thumburl = thumburl+icon
                     thumburl = thumburl+".webp?size=96&quality=lossless"
                     print(thumburl)
-                    embed = await Custom_embed(self.client,thumb=thumburl).setup_embed()
+                    embed = await Custom_embed(self.client,thumb=thumburl,desc="Icon name: "+iconname).setup_embed()
                     embed.set_footer(text=(f'{self.client.user.display_name}'+" | at UTC "f'{timestamp}'), icon_url=f'{self.client.user.avatar}')
                     embed.set_author(name=f'{self.client.get_user(authorid).display_name}'" just found a new icon!", icon_url="https://cdn.discordapp.com/emojis/766701189260771359.webp?size=96&quality=lossless")
                     await announce_channel.send(embed=embed)
