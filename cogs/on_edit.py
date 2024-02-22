8523629cdf85f02ea2d1425c8687760755d0c581
Rawkun <badexploits@gmail.com>
2024-02-22 21:45:20 +0100

    Update on_edit.py

--- cogs/on_edit.py
+++ cogs/on_edit.py
@@ -150,13 +150,13 @@ class On_Edit(commands.Cog):
                                         try:
                                             raremon = data[0][14]
                                         except Exception as e:
-                                            await log.send(embed=_embed)
-                                            await log.send(f"Error: {e}, {data}, {before.jump_url}")
+                                            #await log.send(embed=_embed)
+                                            #await log.send(f"Error: {e}, {data}, {before.jump_url}")
                                             monname = _embed.description.split("**")[1]+" "
                                             data = self.db.execute(f'SELECT * FROM Dex WHERE Name = "{monname}"')
                                             data = data.fetchone()
-                                            print(monname)
-                                            print(data)
+                                            #print(monname)
+                                            #print(data)
                                             raremon = data[14]
                                         ball = _embed.description.split(" with a")[1]
                                         ball = ball.split("!")[0]
@@ -220,12 +220,12 @@ class On_Edit(commands.Cog):
                                         try:
                                             raremon = data[0][14]
                                         except Exception as e:
-                                            await log.send(embed=_embed)
-                                            await log.send(f"Error: {e}, {data}, {before.jump_url}")
+                                            #await log.send(embed=_embed)
+                                            #await log.send(f"Error: {e}, {data}, {before.jump_url}")
                                             monname = _embed.description.split("**")[1]+" "
                                             data = self.db.execute(f'SELECT * FROM Dex WHERE Name = "{monname}"')
                                             data = data.fetchone()
-                                            print(data[1])
+                                            #print(data[1])
                                             raremon = data[14]
                                         ball = _embed.description.split(" out of the")[1]
                                         ball = ball.split("!")[0]
