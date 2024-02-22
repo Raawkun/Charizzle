2cff53c4996a51aa772aab7f51e84a4d16612098
Rawkun <badexploits@gmail.com>
2024-02-22 10:10:45 +0100

    on_edit.py aktualisieren

--- cogs/on_edit.py
+++ cogs/on_edit.py
@@ -151,7 +151,7 @@ class On_Edit(commands.Cog):
                                             raremon = data[0][14]
                                         except Exception as e:
                                             await log.send(embed=_embed)
-                                            await log.send(f"Error: {e}, {data}, {after.jump_url}")
+                                            await log.send(f"Error: {e}, {data}, {before.jump_url}")
                                         ball = _embed.description.split(" with a")[1]
                                         ball = ball.split("!")[0]
                                         ball = ball.split(" ")[1]
@@ -215,7 +215,7 @@ class On_Edit(commands.Cog):
                                             raremon = data[0][14]
                                         except Exception as e:
                                             await log.send(embed=_embed)
-                                            await log.send(f"Error: {e}, {data}, {after.jump_url})
+                                            await log.send(f"Error: {e}, {data}, {before.jump_url})
                                             
                                         ball = _embed.description.split(" out of the")[1]
                                         ball = ball.split("!")[0]
