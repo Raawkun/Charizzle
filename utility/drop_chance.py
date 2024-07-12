32e6a4ee75c93a23391c00e9867acd7c8fde9074
Rawkun <badexploits@gmail.com>
2024-07-13 01:31:12 +0200

    Update drop_chance.py

--- utility/drop_chance.py
+++ utility/drop_chance.py
@@ -3,3 +3,4 @@ rare_calc = {"Common" : 1.2, "Uncommon" : 1.5, "Rare" : 2.3, "SuperRare" : 3.2, 
 ball_used_low = {"Pokeball" : 1.2, "Greatball" : 1.5, "Ultraball" : 2.3, "Premierball" : 3.2, "Diveball" : 2.3, "Masterball" : 4.5}
 ball_used_high = {"Pokeball" : 4.5, "Greatball" : 3.2, "Ultraball" : 2.3, "Premierball" : 1.5, "Diveball" : 2.3, "Masterball" : 1.2}
 buyin = 100000
+standard_rate = 0.2
