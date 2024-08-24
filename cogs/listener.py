317a5eb387c0cf93b37188281f761f638f74488d
Rawkun <badexploits@gmail.com>
2024-08-24 14:59:10 +0200

    listener.py aktualisieren

--- cogs/listener.py
+++ cogs/listener.py
@@ -508,6 +508,8 @@ class Listener(commands.Cog):
                 npc = message.content.split(":")[1]
                 desc = f"{user.mention} </battle:1015311084422434819 npc:{npc}"
                 await message.channel.send(desc, allowed_mentions = disnake.AllowedMentions(users = False))
+                desc = f"{user.mention} </battle npc:{npc}:1015311084422434819"
+                await message.channel.send(desc, allowed_mentions = disnake.AllowedMentions(users = False))
             if "won the battle!" in message.content:
                 #print("Battle won")
                 dataev = self.db.execute(f'SELECT * FROM Admin WHERE Server_ID = {message.guild.id}')
