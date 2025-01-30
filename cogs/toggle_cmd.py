94c7543ec60f3698ee76865989f4f259c066e427
Rawkun <badexploits@gmail.com>
2025-01-30 23:02:40 +0100

    Update toggle_cmd.py

--- cogs/toggle_cmd.py
+++ cogs/toggle_cmd.py
@@ -119,89 +119,92 @@ class Toggle_Cmd(commands.Cog):
 
     @commands.command()
     async def toggle(self, ctx):
-        user_id = ctx.author.id
-        database = self.db.execute(f'SELECT * FROM Toggle WHERE User_ID = {user_id}')
-        database = database.fetchone()
-        author_url = "https://cdn.discordapp.com/emojis/1153729922620215349.webp?size=96&quality=lossless"
-        author_name = ctx.author.display_name
-        gengar_bot = self.client.get_user(1161011648585285652)
-        footer_icon = gengar_bot.display_avatar.url
-        footer_name = gengar_bot.display_name+" I "+self.timestamp
-        emo_yes = ":white_check_mark:"
-        emo_no = ":x:"
-        emo_ping = ":bell:"
-        emo_sile = ":no_bell:"
-        color = 0x807ba6
-        if database:
-            if database[2] == 1:
-                value_grazz = emo_yes
-            else:
-                value_grazz = emo_no
-            if database[3] == 1:
-                value_repel = emo_yes
-            else:
-                value_repel = emo_no
-            if database[4] == 1:
-                value_start = emo_yes
-            else:
-                value_start = emo_no
-            if database[5] == 1:
-                value_link = emo_yes
-            else:
-                value_link = emo_no
-            if database[6] == 0:
-                value_rem = "Text style"
-            elif database[6] == 1:
-                value_rem = "Emote Style"
-            if database[10] == 1:
-                value_spawn = emo_yes
-            elif database[10] == 0:
-                value_spawn = emo_no
-            if database[0][11] == 1:
-                value_fish = emo_yes
-            elif database[11] == 0:
-                value_fish = emo_no
-            if database[12] == 1:
-                value_battle = emo_yes
-            elif database[12] == 0:
-                value_battle = emo_no
-            if database[13] == 1:
-                value_quest = emo_yes
-            elif database[13] == 0:
-                value_quest = emo_no
-            if database[14] == 1:
-                value_questr = emo_yes + emo_sile
-            elif database[14] == 0:
-                value_questr = emo_no
-            elif database[4] == 2:
-                value_questr = emo_yes + emo_ping
-            if database[15] == 1:
-                value_other = emo_yes
-            elif database[15] == 0:
-                value_other = emo_no
-            if database[16] == 0:
-                value_ping = emo_ping
-            else:
-                value_ping = emo_sile
-            embed = disnake.Embed(
-                title="**Settings**", color=color, description="Here you can see your current toggle settings. \nChangeable via ``/toggle`` \n\nThe current settings are:"
-            )
-            embed.set_author(icon_url=author_url,name=author_name)
-            embed.set_footer(icon_url=footer_icon,text=footer_name)
-            embed.add_field(name="Golden Razz Berry/Honey: ",inline=True, value=value_grazz)
-            embed.add_field(name="Repels: ",inline=True, value=value_repel)
-            embed.add_field(name="Starter: ",inline=True, value=value_start)
-            embed.add_field(name="Reminder Mode: ", inline=True, value=value_rem)
-            embed.add_field(name="Pings: ", inline=True, value=value_ping)
-            embed.add_field(name="",inline=True, value="")
-            embed.add_field(name="Spawn: ", inline=True, value=value_spawn)
-            embed.add_field(name="Fish: ", inline=True, value=value_fish)
-            embed.add_field(name="Battle: ", inline=True, value=value_battle)
-            embed.add_field(name="Quest Command: ", inline=True, value=value_quest)
-            embed.add_field(name="Next Quest: ", inline=True, value=value_questr)
-            embed.add_field(name="Other Commands: ", inline=True, value=value_other)
-            embed.set_thumbnail(footer_icon)
-            await ctx.send(embed=embed)
+        try:
+            user_id = ctx.author.id
+            database = self.db.execute(f'SELECT * FROM Toggle WHERE User_ID = {user_id}')
+            database = database.fetchone()
+            author_url = "https://cdn.discordapp.com/emojis/1153729922620215349.webp?size=96&quality=lossless"
+            author_name = ctx.author.display_name
+            gengar_bot = self.client.get_user(1161011648585285652)
+            footer_icon = gengar_bot.display_avatar.url
+            footer_name = gengar_bot.display_name+" I "+self.timestamp
+            emo_yes = ":white_check_mark:"
+            emo_no = ":x:"
+            emo_ping = ":bell:"
+            emo_sile = ":no_bell:"
+            color = 0x807ba6
+            if database:
+                if database[2] == 1:
+                    value_grazz = emo_yes
+                else:
+                    value_grazz = emo_no
+                if database[3] == 1:
+                    value_repel = emo_yes
+                else:
+                    value_repel = emo_no
+                if database[4] == 1:
+                    value_start = emo_yes
+                else:
+                    value_start = emo_no
+                if database[5] == 1:
+                    value_link = emo_yes
+                else:
+                    value_link = emo_no
+                if database[6] == 0:
+                    value_rem = "Text style"
+                elif database[6] == 1:
+                    value_rem = "Emote Style"
+                if database[10] == 1:
+                    value_spawn = emo_yes
+                elif database[10] == 0:
+                    value_spawn = emo_no
+                if database[0][11] == 1:
+                    value_fish = emo_yes
+                elif database[11] == 0:
+                    value_fish = emo_no
+                if database[12] == 1:
+                    value_battle = emo_yes
+                elif database[12] == 0:
+                    value_battle = emo_no
+                if database[13] == 1:
+                    value_quest = emo_yes
+                elif database[13] == 0:
+                    value_quest = emo_no
+                if database[14] == 1:
+                    value_questr = emo_yes + emo_sile
+                elif database[14] == 0:
+                    value_questr = emo_no
+                elif database[4] == 2:
+                    value_questr = emo_yes + emo_ping
+                if database[15] == 1:
+                    value_other = emo_yes
+                elif database[15] == 0:
+                    value_other = emo_no
+                if database[16] == 0:
+                    value_ping = emo_ping
+                else:
+                    value_ping = emo_sile
+                embed = disnake.Embed(
+                    title="**Settings**", color=color, description="Here you can see your current toggle settings. \nChangeable via ``/toggle`` \n\nThe current settings are:"
+                )
+                embed.set_author(icon_url=author_url,name=author_name)
+                embed.set_footer(icon_url=footer_icon,text=footer_name)
+                embed.add_field(name="Golden Razz Berry/Honey: ",inline=True, value=value_grazz)
+                embed.add_field(name="Repels: ",inline=True, value=value_repel)
+                embed.add_field(name="Starter: ",inline=True, value=value_start)
+                embed.add_field(name="Reminder Mode: ", inline=True, value=value_rem)
+                embed.add_field(name="Pings: ", inline=True, value=value_ping)
+                embed.add_field(name="",inline=True, value="")
+                embed.add_field(name="Spawn: ", inline=True, value=value_spawn)
+                embed.add_field(name="Fish: ", inline=True, value=value_fish)
+                embed.add_field(name="Battle: ", inline=True, value=value_battle)
+                embed.add_field(name="Quest Command: ", inline=True, value=value_quest)
+                embed.add_field(name="Next Quest: ", inline=True, value=value_questr)
+                embed.add_field(name="Other Commands: ", inline=True, value=value_other)
+                embed.set_thumbnail(footer_icon)
+                await ctx.send(embed=embed,view=ToggleView(user_id=ctx.author.id))
+        except Exception as e:
+            print(e)
 
 def setup(client):
     client.add_cog(Toggle_Cmd(client))
