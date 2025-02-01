1c5441070f299341751acaf423e9c8529d856ad5
Rawkun <badexploits@gmail.com>
2025-02-01 12:29:45 +0100

    Update listener.py

--- cogs/listener.py
+++ cogs/listener.py
@@ -488,6 +488,7 @@ class Listener(commands.Cog):
                                 async with conn.cursor() as cursor:
                                     await cursor.execute(f"UPDATE Stuff SET Current_Item = {item}")
                                     await cursor.commit()
+                                    await cursor.ensure_closed()
                         except Exception as e:
                             print(f";Promo Error: {e}")
                     if "Research Lab" in _embed.author.name:
